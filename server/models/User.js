bbf8f36b600baece4e0556b7a3369b8140529c4a
ewatki <ewatki.ga@gmail.com>
2024-02-28 00:08:45 -0500

    Added timestamp to User model

--- server/models/User.js
+++ server/models/User.js
@@ -3,12 +3,28 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    timelines: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Timeline' }]
+    username: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    timelines: [
+        { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Timeline' 
+        }
+    ]
+},
+{
+    timestamps: true
 })
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User; 
